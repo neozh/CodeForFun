fbe42791a660dcbac9236fc028f1627f17759d8e
Neo zhang <michael.zhang001@gmail.com>
2017-08-04 22:20:34 +0800

    Update ArrayListMultiThread.java

--- thread/main/com/neo/thread/ArrayListMultiThread.java
+++ thread/main/com/neo/thread/ArrayListMultiThread.java
@@ -1,9 +1,38 @@
 package com.neo.thread;
 
-public class ArrayListMultiThread {
+import java.util.ArrayList;
 
-	public static void main(String[] args) {
+public class ArrayListMultiThread 
+{
+	static ArrayList<Integer> al = new ArrayList<Integer>(10);
+	
+	public static class AddThread implements Runnable
+	{
+		@Override
+		public void run()
+		{
+			for (int i = 0; i < 1000000 ; i++) 
+			{
+				crease( i);
+			}
+		}
 		
+		static synchronized void crease(int i)
+		{
+			al.add(i);
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException 
+	{
+		Thread t1 = new Thread( new AddThread() );
+		
+		Thread t2 = new Thread( new AddThread() );
+		
+		t1.start(); t2.start();
+		t1.join(); 
+		t2.join();
+		System.out.println(al.size());
 
 	}
 
